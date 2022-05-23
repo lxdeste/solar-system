8bbd4d060239859a9b01e98783b77572ca65d3d6
Alex Coetzee <alex@noldor.co.za>
2022-05-23 18:50:24 +0200

    add formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { Grid } from '@mantine/core';
-import React from 'react';
+import { Grid } from "@mantine/core";
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    
     <Grid>
       <Grid.Col span={4}>1</Grid.Col>
       <Grid.Col span={8}>2</Grid.Col>
     </Grid>
-    
   );
 }
 

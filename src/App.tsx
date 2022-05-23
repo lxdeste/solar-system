d502352617d8228b759d954019c341763ad31f7b
Alex Coetzee <alex@noldor.co.za>
2022-05-23 18:49:20 +0200

    setup layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
+import { Grid } from '@mantine/core';
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Grid>
+      <Grid.Col span={4}>1</Grid.Col>
+      <Grid.Col span={8}>2</Grid.Col>
+    </Grid>
+    
   );
 }
 
